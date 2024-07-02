9c4e47058c190bfbc9942632eff5e17204a65d6c
Sendzeagahara <padyshka0@gmail.com>
2024-07-02 17:51:31 +0300

    Score measurements

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 from sys import exit
 import pygame
 
+
+def game_active_score():
+    current_time = (pygame.time.get_ticks() - start_time) // 1000
+    score_surface = main_font.render(f'Gitting Gud score: {current_time}', False, (64, 64, 64))
+    score_rectangle = score_surface.get_rect(topleft=(450, 350))
+    screen.blit(score_surface, score_rectangle)
+
+
+def game_best_score():
+    best_score_surface = main_font.render(f'Best Gitting Gud score: {best_score}', False, (64, 64, 64))
+    best_score_rectangle = best_score_surface.get_rect(topright=(750, 350))
+    screen.blit(best_score_surface, best_score_rectangle)
+
+
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('test-1')
 main_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 clock = pygame.time.Clock()
-game_active = True
+game_active = False
+start_time = 0
+best_score = 0
 
 # backgrounds
 sky_surface = pygame.image.load('graphics/Sky.png').convert_alpha()
 ground_surface = pygame.image.load('graphics/Ground.png').convert_alpha()
 
 # texts
-greet_surface = main_font.render('Runner?', False, (64, 64, 64))
-greet_rectangle = greet_surface.get_rect(center=(400, 50))
-
-game_over_surface_1 = main_font.render('git gud', False, (64, 64, 64))
+game_over_surface_1 = main_font.render('Git Gud', False, (64, 64, 64))
 game_over_rectangle_1 = game_over_surface_1.get_rect(center=(400, 150))
 
-game_over_surface_2 = main_font.render('press "SPACE" to git gud', False, (64, 64, 64))
+game_over_surface_2 = main_font.render('Gress  "SPACE"  to Git Gud', False, (64, 64, 64))
 game_over_rectangle_2 = game_over_surface_2.get_rect(center=(400, 250))
 
 # player
@@ -49,13 +62,13 @@ while True:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     game_active = True
+                    start_time = pygame.time.get_ticks()
 
     if game_active:
         # environment
         screen.blit(sky_surface, (0, 0))
         screen.blit(ground_surface, (0, 300))
-        pygame.draw.rect(screen, '#c0e8ec', greet_rectangle, border_radius=3)
-        screen.blit(greet_surface, greet_rectangle)
+        game_active_score()
 
         # player
         player_gravity += 1
@@ -79,6 +92,8 @@ while True:
         # collisions
         if player_rectangle.colliderect(snail_rectangle) or player_rectangle.colliderect(fly_rectangle):
             game_active = False
+            if best_score < (pygame.time.get_ticks() - start_time) // 1000:
+                best_score = (pygame.time.get_ticks() - start_time) // 1000
             player_rectangle.bottom = 300
             snail_rectangle.x = 600
             fly_rectangle.x = 1000
@@ -86,6 +101,7 @@ while True:
         screen.fill('#c0e8ec')
         screen.blit(game_over_surface_1, game_over_rectangle_1)
         screen.blit(game_over_surface_2, game_over_rectangle_2)
+        game_best_score()
 
     pygame.display.update()
     clock.tick(60)
