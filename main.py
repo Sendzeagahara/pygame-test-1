337ff0b4bc4161a9356c8f2271688e35551ba5ad
Sendzeagahara <padyshka0@gmail.com>
2024-07-03 16:45:46 +0300

    Random enemy spawn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sys import exit
 import pygame
+from random import randint
 
 
 def game_active_score():
@@ -13,6 +14,20 @@ def game_best_score():
     best_score_surface = main_font.render(f'Best Gitting Gud score: {best_score}', False, (64, 64, 64))
     best_score_rectangle = best_score_surface.get_rect(topright=(750, 350))
     screen.blit(best_score_surface, best_score_rectangle)
+
+
+def enemy_movement(enemies):
+    if enemies:
+        for enemy in enemies:
+            enemy.x -= 6
+            if enemy.bottom == 300:
+                screen.blit(snail_surface, enemy)
+            else:
+                screen.blit(fly_surface, enemy)
+        enemies = [enemy for enemy in enemies if enemy.right > 0]
+        return enemies
+    else:
+        return []
 
 
 pygame.init()
@@ -42,11 +57,15 @@ player_gravity = 0
 
 # snail
 snail_surface = pygame.image.load('graphics/Snail/Snail1.png').convert_alpha()
-snail_rectangle = snail_surface.get_rect(midbottom=(600, 300))
 
 # fly
 fly_surface = pygame.image.load('graphics/Fly/Fly1.png').convert_alpha()
-fly_rectangle = fly_surface.get_rect(midbottom=(1000, 200))
+
+enemies_rect_list = []
+
+# timers
+spawn_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(spawn_timer, 1500)
 
 while True:
     # event loop
@@ -64,6 +83,12 @@ while True:
                     game_active = True
                     start_time = pygame.time.get_ticks()
 
+        if event.type == spawn_timer and game_active:
+            if randint(0, 1):
+                enemies_rect_list.append(snail_surface.get_rect(bottomright=(randint(900, 1100), 300)))
+            else:
+                enemies_rect_list.append(fly_surface.get_rect(bottomright=(randint(900, 1100), 200)))
+
     if game_active:
         # environment
         screen.blit(sky_surface, (0, 0))
@@ -77,26 +102,17 @@ while True:
             player_rectangle.bottom = 300
         screen.blit(player_surface, player_rectangle)
 
-        # snail
-        snail_rectangle.x -= 6
-        if snail_rectangle.right <= 0:
-            snail_rectangle.left = 800
-        screen.blit(snail_surface, snail_rectangle)
-
-        # fly
-        fly_rectangle.x -= 6
-        if fly_rectangle.right <= 0:
-            fly_rectangle.left = 800
-        screen.blit(fly_surface, fly_rectangle)
+        # enemy movement
+        enemies_rect_list = enemy_movement(enemies_rect_list)
 
         # collisions
-        if player_rectangle.colliderect(snail_rectangle) or player_rectangle.colliderect(fly_rectangle):
-            game_active = False
-            if best_score < (pygame.time.get_ticks() - start_time) // 1000:
-                best_score = (pygame.time.get_ticks() - start_time) // 1000
-            player_rectangle.bottom = 300
-            snail_rectangle.x = 600
-            fly_rectangle.x = 1000
+        for enemy_rect in enemies_rect_list:
+            if player_rectangle.colliderect(enemy_rect):
+                if best_score < (pygame.time.get_ticks() - start_time) // 1000:
+                    best_score = (pygame.time.get_ticks() - start_time) // 1000
+                player_rectangle.bottom = 300
+                enemies_rect_list = []
+                game_active = False
     else:
         screen.fill('#c0e8ec')
         screen.blit(game_over_surface_1, game_over_rectangle_1)
